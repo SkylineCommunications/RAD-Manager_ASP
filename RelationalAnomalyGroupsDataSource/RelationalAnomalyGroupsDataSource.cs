b4d74cd4839c0426c27f36744fde681d7d472d45
Tobe Deprez <tobe.deprez@skyline.be>
2025-03-11 16:43:18 +0100

    Format parameter keys differently

--- RelationalAnomalyGroupsDataSource/RelationalAnomalyGroupsDataSource.cs
+++ RelationalAnomalyGroupsDataSource/RelationalAnomalyGroupsDataSource.cs
@@ -7,6 +7,7 @@ using Skyline.DataMiner.Analytics.DataTypes;
 using Skyline.DataMiner.Analytics.GenericInterface;
 using Skyline.DataMiner.Analytics.Mad;
 using Skyline.DataMiner.Automation;
+using Skyline.DataMiner.Net.Messages;
 
 namespace RelationalAnomalyGroupsDataSource
 {
@@ -16,10 +17,58 @@ namespace RelationalAnomalyGroupsDataSource
 	public class RelationalAnomalyGroupsDataSource : IGQIDataSource, IGQIOnInit
 	{
 		private GQIDMS dms_;
+		private Dictionary<(int dmaId, int elementId), (string elementName, ParameterInfo[] parameters)> cache_ = new Dictionary<(int dmaId, int elementId), (string elementName, ParameterInfo[] parameters)>();
+
 		public GQIColumn[] GetColumns()
 		{
 			return new GQIColumn[] { new GQIStringColumn("Name"), new GQIIntColumn("DataMiner Id"), new GQIStringColumn("Parameters"),
 				new GQIStringColumn("UpdateModel"), new GQIStringColumn("AnomalyThreshold")};
+		}
+
+		private string InnerParameterKeyToString(ParameterKey pKey, string elementName, ParameterInfo[] parameters)
+		{
+			if (string.IsNullOrEmpty(elementName))
+				elementName = $"{pKey.DataMinerID}/{pKey.ElementID}";
+
+			var parameter = parameters?.FirstOrDefault(p => p.ID == pKey.ParameterID);
+			var parameterName = parameter?.DisplayName ?? pKey.ParameterID.ToString();
+
+			string instance = "";
+			if (!string.IsNullOrEmpty(pKey.DisplayInstance))
+				instance = pKey.DisplayInstance;
+			else
+				instance = pKey.Instance;
+
+			if (string.IsNullOrEmpty(instance))
+				return $"{elementName}/{parameterName}";
+			else
+				return $"{elementName}/{parameterName}/{instance}";
+		}
+
+		private string ParameterKeyToString(ParameterKey pKey)
+		{
+			if (cache_.TryGetValue((pKey.DataMinerID, pKey.ElementID), out var cacheInfo))
+				return InnerParameterKeyToString(pKey, cacheInfo.elementName, cacheInfo.parameters);
+
+			try
+			{
+				var elementRequest = new GetElementByIDMessage(pKey.DataMinerID, pKey.ElementID);
+				var elementResponse = dms_.SendMessage(elementRequest) as ElementInfoEventMessage;
+				if (elementResponse == null)
+					return InnerParameterKeyToString(pKey, null, null);
+
+				var protocolRequest = new GetElementProtocolMessage(pKey.DataMinerID, pKey.ElementID);
+				var protocolResponse = dms_.SendMessage(protocolRequest) as GetElementProtocolResponseMessage;
+				if (protocolResponse == null)
+					return InnerParameterKeyToString(pKey, null, null);
+
+				cache_.Add((pKey.DataMinerID, pKey.ElementID), (elementResponse.Name, protocolResponse.AllParameters));
+				return InnerParameterKeyToString(pKey, elementResponse.Name, protocolResponse.AllParameters);
+			}
+			catch (Exception)
+			{
+				return InnerParameterKeyToString(pKey, null, null);
+			}
 		}
 
 		public GQIPage GetNextPage(GetNextPageInputArgs args)
@@ -33,19 +82,15 @@ namespace RelationalAnomalyGroupsDataSource
 				GetMADParameterGroupInfoMessage msg = new GetMADParameterGroupInfoMessage(groupName);
 				var groupInfoResponse = dms_.SendMessage(msg) as GetMADParameterGroupInfoResponseMessage;
 				MADGroupInfo groupInfo = groupInfoResponse.GroupInfo;
-				string parameterString = "";
-				int dataMinerID = 0;
-				foreach (ParameterKey parameter in groupInfo.Parameters)
-				{
-					parameterString += parameter.ToString() + "; ";
-					dataMinerID = parameter.DataMinerID;
-				}
+				var parameterStr = groupInfo.Parameters.Select(p => ParameterKeyToString(p));
+				int dataMinerID = groupInfo.Parameters.FirstOrDefault()?.DataMinerID ?? 0;
+
 				rows.Add(new GQIRow(
 					new GQICell[]
 					{
 						new GQICell(){Value=groupName,},
 						new GQICell(){Value=dataMinerID,},
-						new GQICell(){Value=parameterString,},
+						new GQICell(){Value=$"[{string.Join(", ", parameterStr)}]",},
 						new GQICell(){Value=groupInfo.UpdateModel.ToString(),},
 						new GQICell(){Value=groupInfo.AnomalyThreshold?.ToString(CultureInfo.InvariantCulture) ?? "3",},
 					}
