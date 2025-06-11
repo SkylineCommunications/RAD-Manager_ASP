e15c5e73a9ced83426b44a7416ebe13f391c644a
Tobe Deprez <tobe.deprez@skyline.be>
2025-06-11 11:21:41 +0200

    Check whether GQI is enabled before installing

--- RAD Manager/RAD Manager.cs
+++ RAD Manager/RAD Manager.cs
@@ -1,36 +1,53 @@
 using System;
-
+using RAD_Manager;
 using Skyline.AppInstaller;
 using Skyline.DataMiner.Automation;
 using Skyline.DataMiner.Net.AppPackages;
+
+internal class GQINotEnabledException : Exception
+{
+	public GQINotEnabledException(string message) : base(message)
+	{
+	}
+}
 
 /// <summary>
 /// DataMiner Script Class.
 /// </summary>
 internal class Script
 {
-    /// <summary>
-    /// The script entry point.
-    /// </summary>
-    /// <param name="engine">Provides access to the Automation engine.</param>
-    /// <param name="context">Provides access to the installation context.</param>
-    [AutomationEntryPoint(AutomationEntryPointType.Types.InstallAppPackage)]
-    public void Install(IEngine engine, AppInstallContext context)
-    {
-        try
-        {
-            engine.Timeout = new TimeSpan(0, 10, 0);
-            engine.GenerateInformation("Starting installation");
-            var installer = new AppInstaller(Engine.SLNetRaw, context);
-            installer.InstallDefaultContent();
+	/// <summary>
+	/// The script entry point.
+	/// </summary>
+	/// <param name="engine">Provides access to the Automation engine.</param>
+	/// <param name="context">Provides access to the installation context.</param>
+	[AutomationEntryPoint(AutomationEntryPointType.Types.InstallAppPackage)]
+	public void Install(IEngine engine, AppInstallContext context)
+	{
+		try
+		{
+			engine.Timeout = new TimeSpan(0, 10, 0);
+			engine.GenerateInformation("Starting installation");
+
+			var gqiEnabled = GQIUtils.IsGqiDxmEnabled();
+			if (!gqiEnabled)
+			{
+				throw new GQINotEnabledException("GQI DxM is not enabled. RAD Manager requires the GQI DxM from version 3.0.0 onwards. " +
+					"Please make sure the GQI DxM is enabled (see https://aka.dataminer.services/gqi-dxm), or use RAD Manager 2.0.4.");
+			}
+
+			var installer = new AppInstaller(Engine.SLNetRaw, context);
+			installer.InstallDefaultContent();
 
 			// Custom installation logic can be added here for each individual install package.
-
-			//TODO: block installation if GQI DxM is not installed (contact Jens Vandewalle)
+		}
+		catch (GQINotEnabledException ex)
+		{
+			engine.ExitFail(ex.Message);
 		}
 		catch (Exception e)
-        {
-            engine.ExitFail($"Exception encountered during installation: {e}");
-        }
-    }
+		{
+			engine.ExitFail($"Exception encountered during installation: {e}");
+		}
+	}
 }
