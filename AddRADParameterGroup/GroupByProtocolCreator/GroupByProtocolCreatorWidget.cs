34bf3e67d5a3c2ad537f816f099773a751292dee
Tobe Deprez <tobe.deprez@skyline.be>
2025-07-16 10:18:37 +0200

    Fix bug in GroupByProtocolCreator that swapped IsValid state between shared model groups and separate groups

--- AddRADParameterGroup/GroupByProtocolCreator/GroupByProtocolCreatorWidget.cs
+++ AddRADParameterGroup/GroupByProtocolCreator/GroupByProtocolCreatorWidget.cs
@@ -250,9 +250,9 @@
 
 			bool hasValidGroup;
 			if (_sharedModelCheckBox?.IsChecked == true)
-				hasValidGroup = groups.Any(g => g.SeparateGroupStatus == SeparateGroupsByProtocolStatus.ValidGroup);
-			else
 				hasValidGroup = groups.Any(g => g.SharedModelGroupStatus == SharedModelGroupByProtocolStatus.ValidSubgroup);
+			else
+				hasValidGroup = groups.Any(g => g.SeparateGroupStatus == SeparateGroupsByProtocolStatus.ValidGroup);
 			IsValid = hasValidGroup;
 
 			if (!IsValid)
